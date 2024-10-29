5d8770d06dae95a771d5be0a881be074987f38d3
Sofiia212121 <sofiiamizina21@gmail.com>
2024-10-29 21:07:08 +0200

    Token verified

--- src/scripts/check_autorized_user.js
+++ src/scripts/check_autorized_user.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    if (getToken()) {
-        redirectToUsersPage();
+    if (!getToken()) {
+        return;
     }
+
+    $.ajax({
+        url: `${apiHost}/current-user`,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + getToken());
+        },
+        success: function (response) {
+            if (response) {
+                redirectToUsersPage();
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        },
+    });
 });
