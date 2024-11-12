01606c86a41f2a94a7407d87ffc7fbd595fe6ded
Sofiia212121 <sofiiamizina21@gmail.com>
2024-11-12 20:53:51 +0200

    API errors displayed

---
+++ src/scripts/form_validation.js
@@ -0,0 +1,50 @@
+function displayFormErrors(errorResponse, formSelector) {
+    if (!errorResponse.validationErrors) {
+        alert(errorResponse.errorMessage);
+        return;
+    }
+
+    const errors = errorResponse.validationErrors;
+
+    const fieldSelectors = getFormFields(formSelector);
+
+    $('.error-message').each(function () {
+        $(this).remove();
+    });
+
+    for (const field in errors) {
+        if (!fieldSelectors[field]) {
+            continue;
+        }
+
+        const inputElement = $(fieldSelectors[field]);
+
+        if (!inputElement) {
+            continue;
+        }
+
+        const parentElement = inputElement.parent();
+
+        errors[field].forEach(errorMessage => {
+            const errorElement = $('<div>', {
+                class: 'error-message text-danger',
+                text: errorMessage
+            });
+
+            parentElement.append(errorElement);
+        });
+    }
+}
+
+function getFormFields(formSelector) {
+    const fieldSelectors = {};
+    $(formSelector).find('input').each(function () {
+        const name = $(this).attr('name');
+        const id = $(this).attr('id');
+        if (name && id) {
+            fieldSelectors[name] = `#${id}`;
+        }
+    });
+
+    return fieldSelectors;
+}
